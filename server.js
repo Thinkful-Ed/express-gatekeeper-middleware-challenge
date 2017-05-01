5e226fcdace2c0f1f241c80f676db7b9c8ec2bf8
System <none>
2017-05-01 19:25:58 +0000

    👚🏅 Add more comments

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 const express = require('express');
+// you'll need to use `queryString` in your `gateKeeper` middleware function
+const queryString = require('query-string');
+
 
 const app = express();
 
+// For this challenge, we're hard coding a list of users, because
+// we haven't learned about databases yet. Normally, you'd store
+// user data in a database, and query the database to find
+// a particular user.
+//
+// ALSO, for this challenge, we're storing user passwords as
+// plain text. This is something you should NEVER EVER EVER 
+// do in a real app. Instead, always use cryptographic
+// password hashing best practices (aka, the tried and true
+// ways to keep user passwords as secure as possible).
+// You can learn mroe about password hashing later
+// here: https://crackstation.net/hashing-security.htm
 const USERS = [
   {id: 1,
    firstName: 'Joe',
@@ -42,7 +57,22 @@ const USERS = [
 ];
 
 
+// write a `gateKeeper` middleware function that:
+//  1. looks for a 'x-username-and-password' request header
+//  2. parses values sent for `user` and `pass` from 'x-username-and-password'
+//  3. looks for a user object matching the sent username and password values
+//  4. if matching user found, add the user object to the request object
+//     (aka, `req.user = matchedUser`)
+function gateKeeper(req, res, next) {
+  // your code should replace the line below
+  next();
+}
 
+// Add the middleware to your app!
+
+// this endpoint returns a json object representing the user making the request,
+// IF they supply valid user credentials. This endpoint assumes that `gateKeeper` 
+// adds the user object to the request if valid credentials were supplied.
 app.get("/api/users/me", (req, res) => {
   // send an error message if no or wrong credentials sent
   if (req.user === undefined) {
